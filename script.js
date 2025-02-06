7e3bb765da4c012e7d66982898999800bb64a32f
rachelle-chengg <183661041+rachelle-chengg@users.noreply.github.com>
2025-02-05 23:31:17 -0800

    updated css

---
+++ script.js
@@ -0,0 +1,6 @@
+function flipCard(card) { /*Defines a function that gets triggered when a card is clicked.*/
+    card.classList.toggle("flip"); 
+    
+    /*If "flip" is NOT present, it adds the "flip" class (causing the card to flip).
+If "flip" is already present, it removes the "flip" class (causing the card to flip back).*/
+}
